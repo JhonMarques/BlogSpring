9c3f49e792a42bd144c08bcf084ebbc354f095d1
JhonMarques <jhonatasbcc@gmail.com>
2021-07-29 21:05:06 -0300

    implementando o form de novo post

--- src/main/java/com/springBoot/springBlog/controller/SpringBlogController.java
+++ src/main/java/com/springBoot/springBlog/controller/SpringBlogController.java
@@ -4,11 +4,15 @@ import com.springBoot.springBlog.model.Post;
 import com.springBoot.springBlog.service.SpringblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -33,5 +37,19 @@ public class SpringBlogController {
         return mv;
     }
 
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "resdirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        springblogService.save(post);
+        return "redirect:posts";
+    }
 
 }
